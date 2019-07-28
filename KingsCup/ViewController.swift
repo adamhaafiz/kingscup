467c198cff70589619d4587e32ec2d5c0b01b8f3
Adam Haafiz <adam@adamhaafiz.com>
2019-07-28 15:27:56 +0100

    Initial Commit

---
+++ KingsCup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KingsCup
+//
+//  Created by Adam Haafiz on 28/07/2019.
+//  Copyright © 2019 Adam Haafiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KingsCupData/KingsCupData.h
@@ -0,0 +1,19 @@
+//
+//  KingsCupData.h
+//  KingsCupData
+//
+//  Created by Adam Haafiz on 28/07/2019.
+//  Copyright © 2019 Adam Haafiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KingsCupData.
+FOUNDATION_EXPORT double KingsCupDataVersionNumber;
+
+//! Project version string for KingsCupData.
+FOUNDATION_EXPORT const unsigned char KingsCupDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KingsCupData/PublicHeader.h>
+
+
